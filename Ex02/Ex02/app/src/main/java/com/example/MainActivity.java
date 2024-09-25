53f6b6b9b6090c6b3ec9e5107d9150f85855a291
Nguyễn Thị Thanh Hiền <152382787+ngthanhhienn@users.noreply.github.com>
2024-09-25 22:09:34 +0700

    1

---
+++ Ex02/Ex02/app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.myapplication.R;
+import com.example.myapplication.Subactivity;
+
+public class MainActivity extends Activity {
+    Button btncall;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"CR424 - onCreat()",Toast.LENGTH_SHORT).show();
+        btncall = findViewById(R.id.btncall);
+        btncall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub
+                Intent intent1 = new Intent(MainActivity.this, Subactivity.class);
+                startActivity(intent1);
+            }
+        });
+    }
+
+    protected void onDestroy() {
+// TODO Auto-generated method stub
+        super.onDestroy();
+        Toast.makeText(this,"CR424 - on Destroy()",Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onPause() {
+// TODO Auto-generated method stub
+        super.onPause();
+        Toast.makeText(this," CR424 - onPause", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onRestart() {
+// TODO Auto-generated method stub
+        super.onRestart();
+        Toast.makeText(this," CR424 - onRestart", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onResume() {
+// TODO Auto-generated method stub12
+        super.onResume();
+        Toast.makeText(this," CR424 - onResume", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onStart() {
+// TODO Auto-generated method stub
+        super.onStart();
+        Toast.makeText(this," CR424 - onStart", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onStop() {
+// TODO Auto-generated method stub
+        super.onStop();
+        Toast.makeText(this," CR424 - onStop", Toast.LENGTH_SHORT).show();
+    }}
